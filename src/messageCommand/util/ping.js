f683e01ff964f9acc5dc2113c324a1c68a2d6c9d
aho-emi <136156874+aho-emi@users.noreply.github.com>
2023-06-11 00:08:21 +0545

    Create ping.js

---
+++ src/messageCommand/util/ping.js
@@ -0,0 +1,9 @@
+module.exports={
+  name: "latency",
+  aliases: ["pong", "pang", "ping"],
+  description: "Shows the latency of bot",
+  usage: "{prefix} ping",
+  code:`
+  Bot's Latency: **$pingMs**
+  `
+}
