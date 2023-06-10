329eeeb941cf6e63399ab08313df475aaea84445
aho-emi <136156874+aho-emi@users.noreply.github.com>
2023-06-10 14:52:27 +0545

    Create modalExample.js

---
+++ src/messageCommand/modalExample.js
@@ -0,0 +1,15 @@
+module.exports=[{
+  name: "modal",
+  code:`
+
+  $interactionModal[Modal;modalID;{actionRow:
+    {textInput:What's your name?:1:nameInput:true:Your pretty name!:2:200}
+  }
+  {actionRow:
+    {textInput:What's your age?:1:ageInput:true:You young soul!:1:3}
+  }]
+  
+`
+}]
+
+// You can use the interaction code in same file or in interaction folder //
