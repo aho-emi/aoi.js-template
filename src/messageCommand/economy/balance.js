970d1aec914a7c9d093897c6d8701069e13f9ad6
aho-emi <136156874+aho-emi@users.noreply.github.com>
2023-06-10 23:43:41 +0545

    Update balance.js

--- src/messageCommand/economy/balance.js
+++ src/messageCommand/economy/balance.js
@@ -1,1 +1,33 @@
+module.exports=[{
+  name: "balance",
+  aliases: ["bal", "money"],
+  description: "Shows the balance of a user",
+  usage: "{prefix} balance / {prefix} balance @user",
+  code:`
+  
+  $title[$username[$get[user]]'s Balance]
+  $color[00ffff]
+  $thumbnail[$userAvatar[$get[user]]]
+  $addField[Bank;> $numberSeparator[$getGlobalUserVar[bank;$get[user]];,]$$]
+  $addField[Wallet;> $numberSeparator[$getGlobalUserVar[wallet;$get[user]];,]$$]
 
+  $let[user;$findUser[$msssage]]
+  ` // This is global balance meaning it will show same amount in every server
+},{
+  name: "serverBalance",
+  aliases: ["sbal", "gmoney"],
+  description: "Shows the balance of a user in a server",
+  usage: "{prefix} serverbalance / {prefix} sbal @user",
+  code:`
+  
+  $title[$username[$get[user]]'s Balance]
+  $color[00ffff]
+  $thumbnail[$userAvatar[$get[user]]]
+  $addField[Bank;> $numberSeparator[$getUserVar[bank;$get[user]];,]$$]
+  $addField[Wallet;> $numberSeparator[$getUserVar[wallet;$get[user]];,]$$]
+
+  $let[user;$findUser[$msssage]]
+  ` // This is user guild balance meaning it will show different amount in every server
+}]
+
+// You might be thinking how to use this so basically you use $setGlobalUserVar or $setUserVar corresponding to which one you want to use.
