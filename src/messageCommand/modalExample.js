8be5ba90934c70ab41d7a2510a35b80ce3076ced
aho-emi <136156874+aho-emi@users.noreply.github.com>
2023-06-10 15:09:09 +0545

    Update modalExample.js

--- src/messageCommand/modalExample.js
+++ src/messageCommand/modalExample.js
@@ -1,5 +1,17 @@
 module.exports=[{
+  name: "interactionModal",
+  code: `
+  
+$addButton[1;Press Me!;primary;modal;false]
+Pretty Example!
+
+`
+  // // Create a button which will trigger the modal.
+
+},{
   name: "modal",
+  type: "interaction",
+  prototype: "button", // Using "prototype" as this interaction belongs to a button.
   code:`
 
   $interactionModal[Modal;modalID;{actionRow:
@@ -8,8 +20,24 @@ module.exports=[{
   {actionRow:
     {textInput:What's your age?:1:ageInput:true:You young soul!:1:3}
   }]
-  
 `
-}]
+  /* The modal itself, we use {textInput} to accept user input.
 
-// You can use the interaction code in same file or in interaction folder //
+The correct usage of {textInput} would be:
+
+{textInput:title:type (1 : small text field, 2 : big text field):customID:required ( true, false ):placeholder:minVal:maxVal}
+
+*/
+},{
+  name: "modal",
+  type: "interaction",
+  prototype: "modal", // Using "prototype" as this interaction belongs to a modal.
+  code: `
+$interactionReply[Thanks for submitting this form!;;;;everyone;true]
+
+$title[$username submitted a form!;$userAvatar]
+$addField[Their name is..;$textInputValue[nameInput]]
+$addField[Their age is..;$textInputValue[ageInput]]
+`
+  // Using $textInputValue you can retrieve the input value from earlier modal
+}]
