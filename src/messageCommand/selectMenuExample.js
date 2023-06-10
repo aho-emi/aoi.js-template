6197b0d8dd34fb30ee7a019bb3c5385052d4f285
aho-emi <136156874+aho-emi@users.noreply.github.com>
2023-06-10 15:05:09 +0545

    Update selectMenuExample.js

--- src/messageCommand/selectMenuExample.js
+++ src/messageCommand/selectMenuExample.js
@@ -13,4 +13,25 @@ module.exports=[{
   $addSelectMenu[1;selectMenuAuthor;Place Holder!;1;1;false;Option A:Description of option A:optionA.$authorID:false;Option B:Description of option B:optipnB.$authorID:false]
 
   ` // This is author only select menu
+},{
+  name: "selectMenu",
+  type: "interaction",
+  prototype: "selectMenu", // Using "prototype" as this interaction belongs to a select menu.
+  code:`
+  
+  $interactionReply[You selected an option.]
+  
+  ` // This is normal select menu interaction.
+},{
+  name: "selectMenuAuthor",
+  type: "interaction",
+  prototype: "selectMenu", // Using "prototype" as this interaction belongs to a select menu.
+  code:`
+  
+  $interactionReply[You selected an option.]
+
+  $onlyIf[$splitText[2]==$authorID]
+  $textSplit[$interactionData[values[0]];.]
+  
+  ` // This is author only selet menu interaction.
 }]
