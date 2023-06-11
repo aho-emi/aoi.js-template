4816d71200735f2b668da028dbeb2e1ae0199ccc
Emi <136156874+aho-emi@users.noreply.github.com>
2023-06-12 05:08:51 +0545

    Delete ping.js

--- src/messageCommand/ping.js
+++
@@ -1,9 +0,0 @@
-module.exports={
-  name: "ping",
-  aliases: ["pong", "pang"], // or "ping"
-  description: "Shows the latency of bot",
-  usage: "{prefix} ping",
-  code:`
-  Bot's Latency: **$pingMs**
-  `
-}
