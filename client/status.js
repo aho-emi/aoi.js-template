e57590df112c25292d11b459b9700f6b3548337a
aho-emi <136156874+aho-emi@users.noreply.github.com>
2023-06-11 07:22:06 +0545

    Update status.js

--- client/status.js
+++ client/status.js
@@ -1,1 +1,12 @@
-
+module.exports={
+  time: 12,
+  statuses:[{
+    text: "Emi's heartbeat ❤️", 
+    type: "LISTENING",
+    presence: "dnd"
+  },{
+    text: "Emi's Emotions 🥺", 
+    type: "PLAYING",
+    presence: "idle"
+  }]
+}
