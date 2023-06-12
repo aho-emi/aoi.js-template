b81f95c1f033e4a9bdd7a13da81c2c562f488f76
Emi <136156874+aho-emi@users.noreply.github.com>
2023-06-12 20:57:08 +0545

    Update loop.js

--- src/messageCommand/examples/loop.js
+++ src/messageCommand/examples/loop.js
@@ -1,14 +1,17 @@
-// This will loop the message for unlimited amount of time
+/* // This will loop the message for unlimited amount of time
 module.exports = [{
     name: "loop",
     type: "loop",
-    channel: "channelID",
+    channel: "1101580123788017724", // Will only work if bot has access ti that channel
     executeOnStartup: false, // This will determine whether the code will execute when bot gets online
     every: 10000, // This will run the code every 10 seconds
     code: `
 This is a loop`
 }]
 
-/* Remember $loop and this loop works alike but are not same.
-$loop executes a awaited cmd for given time
-This loop executes a given code for infinite time. */
+// Remember $loop and this loop works alike but are not same. $loop executes an awaited cmd for given time
+// This loop executes a given code for infinite time.
+
+// I am commenting this because i dont know executeOnStartup: false does work properly
+
+*/
